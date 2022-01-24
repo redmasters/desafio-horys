c7fd03d0b4a3b0f6f07305764cb3d69f0b1b09eb
redmasters <erasmo.cjunior@gmail.com>
2022-01-24 17:44:54 -0300

    Adicionando rotina ops e eureka-server

--- desafio/java8/api-produto/src/main/java/br/com/horys/apiproduto/ApiProdutoApplication.java
+++ desafio/java8/api-produto/src/main/java/br/com/horys/apiproduto/ApiProdutoApplication.java
@@ -3,8 +3,10 @@ package br.com.horys.apiproduto;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableRabbit
+@EnableEurekaClient
 @SpringBootApplication
 public class ApiProdutoApplication {
     public static void main(String[] args) {
